74c9cba3093b276e5416b0a4aa6d8c033f8b9acd
saravenpi <saravenpi@pop-os.localdomain>
2022-02-03 00:10:20 +0100

    connexion to database with register endpoint and env variables management

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
 namespace door
@@ -11,33 +14,65 @@ namespace door
         public String success { get; set; }
         public String message { get; set; }
     }
+    public class User
+         {
+             
+             public String username { get; set; }
+             public String password { get; set; }
+             
+             public String avatar { get; set; }
+             
+             public String bio { get; set; }
+             
+             public String banner { get; set; }
+             
+             public String color { get; set; }
+             
+             public List<User> friends { get; set; }
+             
+             public List<String> circles { get; set; }
+
+             [BsonConstructor]
+             public User(string username, string password)
+             {
+                 this.username = username;
+                 this.password = password;
+                 this.avatar = "https://i.imgur.com/k7eDNwW.jpg";
+                 this.bio = "Hey, I'm using Versine!";
+                 this.banner = "https://images7.alphacoders.com/421/thumb-1920-421957.jpg";
+                 this.color = "28DBB7";
+                 this.friends = new List<User>();
+                 this.circles = new List<string>();
+             }
+
+         }
     class HttpServer
     {
         
-        public static HttpListener listener;
-        public static string url = "http://localhost:8000/";
+        public static HttpListener? Listener;
 
-        public static async Task HandleIncomingConnections()
+        public static async Task HandleIncomingConnections(IConfigurationRoot config)
         {
             // Replace the uri string with your MongoDB deployment's connection string.
-            
-            
-            /* var connectionString = "";
+
+            var connectionString = config.GetValue<String>("MongoDB");
             var client = new MongoClient(
                 connectionString
             );
-            var database = client.GetDatabase("UserDB");
-            var collection = database.GetCollection<BsonDocument>("users");
-            */
             
-            
+            BsonClassMap.RegisterClassMap<User>();
+            var database = client.GetDatabase("UsersDB");
+            var collection = database.GetCollection<User>("users");
             Console.WriteLine("Database connected");
+            
+            
+            
             
             while (true)
             {
                 // While a user hasn't visited the `shutdown` url, keep on handling requests
                 // Will wait here until we hear from a connection
-                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerContext ctx = await Listener?.GetContextAsync()!;
 
                 // Peel out the requests and response objects
                 HttpListenerRequest req = ctx.Request;
@@ -45,36 +80,89 @@ namespace door
 
                 // Print out some info about the request
                 Console.WriteLine(req.HttpMethod);
-                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.Url?.ToString());
                 Console.WriteLine(req.UserHostName);
                 Console.WriteLine(req.UserAgent);
 
                 
                 // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
-                if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/login"))
+                if ((req.HttpMethod == "POST") && (req.Url?.AbsolutePath == "/register"))
                 {
                     var reader = new StreamReader(req.InputStream);
-                    string bodyString= reader.ReadToEnd();
-                    
+                    string bodyString = await reader.ReadToEndAsync();
                     dynamic body = JsonConvert.DeserializeObject(bodyString);
-                    Console.WriteLine(body);
-                    
-                    var response = new Response
+                    string username = body.username;
+                    string password = body.password;
+                    if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) &&
+                        !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password))
                     {
-                        success = "true",
-                        message = "login requested"
-                    };
-                    
-                    string jsonString = JsonConvert.SerializeObject(response);
-                    byte[] data = Encoding.UTF8.GetBytes(jsonString);
-                    
-                    resp.ContentType = "application/json";
-                    resp.ContentEncoding = Encoding.UTF8;
-                    resp.ContentLength64 = data.LongLength;
-                    
-                    // Write out to the response stream (asynchronously), then close it
-                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
-                    resp.Close();
+                        var filter = Builders<User>.Filter.Eq("username", username);
+                        var documents = collection.Find(filter);
+    
+                        if (await documents.CountDocumentsAsync() < 1)
+                        {
+                            Console.WriteLine("No user found with username: " + username);
+                            var newuser = new User(username, password);
+                            await collection.InsertOneAsync(newuser);                       
+                            Console.WriteLine("User created");
+                            var response = new Response
+                            {
+                                success = "true",
+                                message = "user created"
+                            };
+                            
+                            string jsonString = JsonConvert.SerializeObject(response);
+                            byte[] data = Encoding.UTF8.GetBytes(jsonString);
+                            
+                            resp.ContentType = "application/json";
+                            resp.ContentEncoding = Encoding.UTF8;
+                            resp.ContentLength64 = data.LongLength;
+                            
+                            // Write out to the response stream (asynchronously), then close it
+                            await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                            resp.Close();    
+                        }
+                        else
+                        {
+                            Console.WriteLine("A user already exists with this username");
+                            var response = new Response
+                            {
+                                success = "false",
+                                message = "username taken"
+                            };
+                            
+                            string jsonString = JsonConvert.SerializeObject(response);
+                            byte[] data = Encoding.UTF8.GetBytes(jsonString);
+                            
+                            resp.ContentType = "application/json";
+                            resp.ContentEncoding = Encoding.UTF8;
+                            resp.ContentLength64 = data.LongLength;
+                            
+                            // Write out to the response stream (asynchronously), then close it
+                            await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                            resp.Close();    
+                        }    
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid body");
+                        var response = new Response
+                        {
+                            success = "false",
+                            message = "invalid body"
+                        };
+                        
+                        string jsonString = JsonConvert.SerializeObject(response);
+                        byte[] data = Encoding.UTF8.GetBytes(jsonString);
+                        
+                        resp.ContentType = "application/json";
+                        resp.ContentEncoding = Encoding.UTF8;
+                        resp.ContentLength64 = data.LongLength;
+                        
+                        // Write out to the response stream (asynchronously), then close it
+                        await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                        resp.Close();    
+                    }
                 }
                 else
                 {
@@ -102,18 +190,25 @@ namespace door
         public static void Main(string[] args)
         {
             // Create a Http server and start listening for incoming connections
+            var config =
+                new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true)
+                    .AddEnvironmentVariables()
+                    .Build();
             
-            listener = new HttpListener();
-            listener.Prefixes.Add(url);
-            listener.Start();
+            var url = "http://*:" + config.GetValue<String>("Port") + "/";
+            Listener = new HttpListener();
+            Listener.Prefixes.Add(url);
+            Listener.Start();
             Console.WriteLine("Listening for connections on {0}", url);
 
             // Handle requests
-            Task listenTask = HandleIncomingConnections();
+            Task listenTask = HandleIncomingConnections(config);
             listenTask.GetAwaiter().GetResult();
 
             // Close the listener
-            listener.Close();
+            Listener.Close();
         }
     }
 }
