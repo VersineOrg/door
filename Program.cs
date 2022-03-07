6638d7f8a5dcfbb3fe3ce8f6f5daeb1ee7ed8fdc
Saravenpi <saravenpi@protonmail.com>
2022-03-07 22:47:36 +0100

    Revert "jsp"
    This reverts commit 49dca3a1395f5314fbf4c8b96d18a53e417b2229.

--- Program.cs
+++ Program.cs
@@ -6,100 +6,101 @@ namespace door;
 
 class HttpServer
 {
+        
+    public static HttpListener? Listener;
 
-  public static HttpListener? Listener;
-
-  public static async Task HandleIncomingConnections(Database database)
-  {
-    while (true)
+    public static async Task HandleIncomingConnections(Database database)
     {
-      HttpListenerContext ctx = await Listener?.GetContextAsync()!;
-
-      HttpListenerRequest req = ctx.Request;
-      HttpListenerResponse resp = ctx.Response;
-
-      Console.WriteLine(req.HttpMethod);
-      Console.WriteLine(req.Url?.ToString());
-      Console.WriteLine(req.UserHostName);
-      Console.WriteLine(req.UserAgent);
-
-      if (req.HttpMethod == "POST" && req.Url?.AbsolutePath == "/register")
-      {
-        StreamReader reader = new StreamReader(req.InputStream);
-        string bodyString = await reader.ReadToEndAsync();
-        dynamic body = JsonConvert.DeserializeObject(bodyString)!;
-
-        string username = (string) body.username;
-        string password = (string) body.password;
-        string ticket = (string) body.ticket;
-
-        if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) &&
-            !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password) && !String.IsNullOrEmpty(ticket) && !String.IsNullOrWhiteSpace(ticket))
+        while (true)
         {
-          if (!database.UserExists(username))
-          {
-            if (database.TicketIsValid(ticket))
+            HttpListenerContext ctx = await Listener?.GetContextAsync()!;
+
+            HttpListenerRequest req = ctx.Request;
+            HttpListenerResponse resp = ctx.Response;
+                
+            Console.WriteLine(req.HttpMethod);
+            Console.WriteLine(req.Url?.ToString());
+            Console.WriteLine(req.UserHostName);
+            Console.WriteLine(req.UserAgent);
+
+            if (req.HttpMethod == "POST" && req.Url?.AbsolutePath == "/register")
             {
-              if (database.RegisterUser(username, password, ticket))
-              {
-                Response.Success(resp, "user created", "");
-              }
-              else
-              {
-                Response.Fail(resp,"cannot register user");
-              }
+                StreamReader reader = new StreamReader(req.InputStream);
+                string bodyString = await reader.ReadToEndAsync();
+                dynamic body = JsonConvert.DeserializeObject(bodyString)!;
+                    
+                string username = (string) body.username;
+                string password = (string) body.password;
+                string ticket = (string) body.ticket;
+                    
+                if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) &&
+                    !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password) && !String.IsNullOrEmpty(ticket) && !String.IsNullOrWhiteSpace(ticket))
+                {
+                    if (!database.UserExists(username))
+                    {
+                        if (database.TicketIsValid(ticket))
+                        {
+                            if (database.RegisterUser(username, password, ticket))
+                            {
+                                Response.Success(resp, "user created", "");
+                            }
+                            else
+                            {
+                                Response.Fail(resp,"cannot register user");
+                            }
+                        }
+                        else
+                        {
+                            Response.Fail(resp,"invalid or expired ticket");
+                        }
+                    }
+                    else
+                    {
+                        Response.Fail(resp, "username taken");
+                    }    
+                }
+                else
+                {
+                    Response.Fail(resp,"invalid body");
+                }
+            }
+
+            if (req.HttpMethod == "POST" && req.Url?.AbsolutePath == "/login")
+            {
+                StreamReader reader = new StreamReader(req.InputStream);
+                string bodyString = await reader.ReadToEndAsync();
+                dynamic body = JsonConvert.DeserializeObject(bodyString)!;
+                    
+                string username = body.username;
+                string password = body.password;
+                    
+                if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) &&
+                    !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password))
+                {
+                    string? token = database.AuthenticateUser(username, password);
+                    if (token != null)
+                    {
+                        Response.Success(resp, "user logged", token);
+                    }
+                    else
+                    {
+                        Response.Fail(resp, "wrong password or username");
+                    }
+                }
+                else
+                {
+                    Response.Fail(resp, "invalid body");
+                }
             }
             else
             {
-              Response.Fail(resp,"invalid or expired ticket");
+                Response.Fail(resp, "404");
             }
-          }
-          else
-          {
-            Response.Fail(resp, "username taken");
-          }    
         }
-        else
-        {
-          Response.Fail(resp,"invalid body");
-        }
-      }
-
-      if (req.HttpMethod == "POST" && req.Url?.AbsolutePath == "/login")
-      {
-        StreamReader reader = new StreamReader(req.InputStream);
-        string bodyString = await reader.ReadToEndAsync();
-        dynamic body = JsonConvert.DeserializeObject(bodyString)!;
-
-        string username = body.username;
-        string password = body.password;
-
-        if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) &&
-            !String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password))
-        {
-          string? token = database.AuthenticateUser(username, password);
-          if (token != null)
-          {
-            Response.Success(resp, "user logged", token);
-          }
-          else
-          {
-            Response.Fail(resp, "wrong password or username");
-          }
-        }
-        else
-        {
-          Response.Fail(resp, "invalid body");
-        }
-      }
-      else
-      {
-        Response.Fail(resp, "404");
-      }
     }
-  }
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   public static void Main(string[] args)
   {
@@ -114,6 +115,19 @@ class HttpServer
         // Create a new 
         EasyMango.EasyMango database = new EasyMango.EasyMango(connectionString,databaseNAme,collectionName);
         
+=======
+    public static void Main(string[] args)
+    {
+        IConfigurationRoot config =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+            
+        string connectionString = config.GetValue<String>("MongoDB");
+        Database database = new Database(connectionString);
+>>>>>>> parent of 49dca3a (jsp)
             
         // Create a Http server and start listening for incoming connections
         string url = "http://*:" + config.GetValue<String>("Port") + "/";
@@ -121,11 +135,16 @@ class HttpServer
         Listener.Prefixes.Add(url);
         Listener.Start();
         Console.WriteLine("Listening for connections on {0}", url);
+<<<<<<< HEAD
 >>>>>>> parent of 62fdd10 (added comment)
+=======
+>>>>>>> parent of 49dca3a (jsp)
 
-    string connectionString = config.GetValue<String>("MongoDB");
-    Database database = new Database(connectionString);
+        // Handle requests
+        Task listenTask = HandleIncomingConnections(database);
+        listenTask.GetAwaiter().GetResult();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     // Create a Http server and start listening for incoming connections
     string url = "http://*:" + config.GetValue<String>("Port") + "/";
@@ -148,3 +167,9 @@ class HttpServer
     }
 }
 >>>>>>> parent of 62fdd10 (added comment)
+=======
+        // Close the listener
+        Listener.Close();
+    }
+}
+>>>>>>> parent of 49dca3a (jsp)
